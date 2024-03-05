7f0022bfc23973c9438b79035cc67a6e39775a51
Thomas Rubin <thomas.rubin2@protonmail.com>
2024-03-05 23:50:30 +0100

    Change default pins of I2C and reworking SERIAL_PRINT makro to work.

--- Arduino/Theraband_ATET/Theraband_ATET.ino
+++ Arduino/Theraband_ATET/Theraband_ATET.ino
@@ -3,6 +3,8 @@
 
 Adafruit_VL6180X vl = Adafruit_VL6180X();
 
+TwoWire *wire = new TwoWire(20,21);
+
 #define LED_PIN_YELLOW      10
 #define LED_PIN_GREEN       16
 #define LED_PIN_RED         17
@@ -10,74 +12,81 @@ Adafruit_VL6180X vl = Adafruit_VL6180X();
 #define RANGE_LOW_THRESHOLD 65
 #define RANGE_HIGH_THRESHOLD 85
 
-#define PRINT_SERIAL(Text) if(isSerial) Serial.println(#Text);
+#define SERIAL_PRINT_TEXT(text) if(isSerial) Serial.println(#text);
 
 bool isSerial = true;
 
 void setup() {
   Serial.begin(115200);
 
-  uint8_t serialCount = 0;
+  uint16_t serialCount = 0;
   while (!Serial) {
-    if(serialCount++ > 100) {
+    if(serialCount++ > 1000) {
       isSerial = false;
       break;
     }
     delay(1);
   }
 
+
+
   pinMode(LED_PIN_YELLOW, OUTPUT);
   pinMode(LED_PIN_GREEN, OUTPUT);
   pinMode(LED_PIN_RED, OUTPUT);
 
-  PRINT_SERIAL("Adafruit VL6180x test!");
-  if (! vl.begin()) {
-    PRINT_SERIAL("Failed to find sensor");
+  SERIAL_PRINT_TEXT("Adafruit VL6180x test!"); 
+
+  
+
+  if (! vl.begin(wire)) {
+    SERIAL_PRINT_TEXT("Failed to find sensor");
     while (1);
   }
-  PRINT_SERIAL("Sensor found!");
+  SERIAL_PRINT_TEXT("Sensor found!");
 }
 
 void loop() {
   float lux = vl.readLux(VL6180X_ALS_GAIN_5);
 
-  Serial.print("Lux: "); PRINT_SERIAL(lux);
+  if (isSerial) {Serial.print("Lux: "); Serial.println(lux);}
   
   uint8_t range = vl.readRange();
   uint8_t status = vl.readRangeStatus();
 
-  if (status == VL6180X_ERROR_NONE) {
-    Serial.print("Range: "); PRINT_SERIAL(range);
-  }
+  if (isSerial) {
+    if (status == VL6180X_ERROR_NONE) {
+      Serial.print("Range: "); Serial.println(range);
+    }
 
-  // Some error occurred, print it out!
-  
-  if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
-    PRINT_SERIAL("System error");
-  }
-  else if (status == VL6180X_ERROR_ECEFAIL) {
-    PRINT_SERIAL("ECE failure");
-  }
-  else if (status == VL6180X_ERROR_NOCONVERGE) {
-    PRINT_SERIAL("No convergence");
-  }
-  else if (status == VL6180X_ERROR_RANGEIGNORE) {
-    PRINT_SERIAL("Ignoring range");
-  }
-  else if (status == VL6180X_ERROR_SNR) {
-    PRINT_SERIAL("Signal/Noise error");
-  }
-  else if (status == VL6180X_ERROR_RAWUFLOW) {
-    PRINT_SERIAL("Raw reading underflow");
-  }
-  else if (status == VL6180X_ERROR_RAWOFLOW) {
-    PRINT_SERIAL("Raw reading overflow");
-  }
-  else if (status == VL6180X_ERROR_RANGEUFLOW) {
-    PRINT_SERIAL("Range reading underflow");
-  }
-  else if (status == VL6180X_ERROR_RANGEOFLOW) {
-    PRINT_SERIAL("Range reading overflow");
+    // Some error occurred, print it out!
+    
+    if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
+      Serial.println("System error");
+    }
+    else if (status == VL6180X_ERROR_ECEFAIL) {
+      Serial.println("ECE failure");
+    }
+    else if (status == VL6180X_ERROR_NOCONVERGE) {
+      Serial.println("No convergence");
+    }
+    else if (status == VL6180X_ERROR_RANGEIGNORE) {
+      Serial.println("Ignoring range");
+    }
+    else if (status == VL6180X_ERROR_SNR) {
+      Serial.println("Signal/Noise error");
+    }
+    else if (status == VL6180X_ERROR_RAWUFLOW) {
+      Serial.println("Raw reading underflow");
+    }
+    else if (status == VL6180X_ERROR_RAWOFLOW) {
+      Serial.println("Raw reading overflow");
+    }
+    else if (status == VL6180X_ERROR_RANGEUFLOW) {
+      Serial.println("Range reading underflow");
+    }
+    else if (status == VL6180X_ERROR_RANGEOFLOW) {
+      Serial.println("Range reading overflow");
+    }
   }
 
   if (range < RANGE_LOW_THRESHOLD) {
